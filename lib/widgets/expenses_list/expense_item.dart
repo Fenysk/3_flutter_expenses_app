939ff48302647c86621844d605f8f054f63ca4ec
Fenysk <minefenysk@gmail.com>
2024-05-15 18:58:12 +0200

    🚧 Displaying data on ExpenseItem Widget (card)

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -9,7 +9,34 @@ class ExpenseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(expense.title),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        child: Column(
+          children: [
+            Text(
+              expense.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    Text(expense.category.name.toUpperCase()),
+                    const SizedBox(width: 8),
+                    Text(expense.createdAt.toString())
+                  ],
+                ),
+                const Spacer(),
+                Text('€ ${expense.amount.toStringAsFixed(2)}'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
