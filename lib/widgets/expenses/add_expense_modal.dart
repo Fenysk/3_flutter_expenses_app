88db1342c9d00528938a64c5a2ae293e3f89ace2
Fenysk <minefenysk@gmail.com>
2024-05-15 20:59:40 +0200

    🚧 Adding TextField

--- lib/widgets/expenses/add_expense_modal.dart
+++ lib/widgets/expenses/add_expense_modal.dart
@@ -8,13 +8,37 @@ class AddExpenseModal extends StatefulWidget {
 }
 
 class _AddExpenseModalState extends State<AddExpenseModal> {
+  String _enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 300,
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-      child: const Text('Salut'),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            onChanged: _saveTitleInput,
+            decoration: const InputDecoration(
+              label: Text('Sujet de la dépense'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_enteredTitle);
+                },
+                child: const Text('Save Expense'),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
