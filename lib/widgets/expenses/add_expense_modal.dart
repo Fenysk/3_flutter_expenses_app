a5b60b57bbd424025cf7ca29a3547a9668c048db
Fenysk <minefenysk@gmail.com>
2024-05-15 21:41:34 +0200

    🚧 Adding DatePicker

--- lib/widgets/expenses/add_expense_modal.dart
+++ lib/widgets/expenses/add_expense_modal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expenses_app/models/expense.dart';
 
 class AddExpenseModal extends StatefulWidget {
   const AddExpenseModal({super.key});
@@ -10,6 +11,23 @@ class AddExpenseModal extends StatefulWidget {
 class _AddExpenseModalState extends State<AddExpenseModal> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -31,16 +49,42 @@ class _AddExpenseModalState extends State<AddExpenseModal> {
             ),
             maxLength: 50,
           ),
-          TextField(
-            controller: _amountController,
-            decoration: const InputDecoration(
-              label: Text('Montant'),
-              prefixText: '€ ',
-            ),
-            keyboardType: const TextInputType.numberWithOptions(
-              signed: false,
-              decimal: false,
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  decoration: const InputDecoration(
+                    label: Text('Montant'),
+                    prefixText: '€ ',
+                  ),
+                  keyboardType: const TextInputType.numberWithOptions(
+                    signed: false,
+                    decimal: false,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'Aucune date'
+                          : '${formatter.format(_selectedDate!)}',
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Row(
             children: [
@@ -55,6 +99,8 @@ class _AddExpenseModalState extends State<AddExpenseModal> {
                 onPressed: () {
                   print(_titleController.text);
                   print(_amountController.text);
+                  if (_selectedDate != null)
+                    print(formatter.format(_selectedDate!));
                   Navigator.pop(context);
                 },
                 child: const Text('Enregistrer la dépense'),
