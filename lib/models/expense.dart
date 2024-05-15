de1883c93146d2c2b78a73b1fc32e666291fd5e9
Fenysk <minefenysk@gmail.com>
2024-05-15 17:42:37 +0200

    🎨 Use enum

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -4,15 +4,19 @@ const uuid = Uuid();
 
 String get _generatedNewId => 'exps_${uuid.v4().replaceAll('-', '')}';
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.createdAt,
+    required this.category,
   }) : id = _generatedNewId;
 
   final String id; // exps_12345678
   final String title; // Assurance
   final double amount; // 45.50
   final DateTime createdAt;
+  final Category category; // leisure Expense(categ)
 }
